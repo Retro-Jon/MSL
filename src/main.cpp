db319af635d4273d37c825a3b83a0f17ca96b89c
Jonathan Buckel <jonathanbuckel38@gmail.com>
2024-09-13 20:52:02 -0400

    Added commands to REPL

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include "lang.hpp"
+#include <cctype>
 #include <iostream>
+#include <string>
+#include <algorithm>
 
 int main(int argc, char** argv)
 {
@@ -33,11 +36,71 @@ int main(int argc, char** argv)
         std::vector<Token> stack;
         std::cout << "REPL Mode" << std::endl;
         std::string input;
+        std::map<int, std::string> code;
 
         do
         {
             std::cout << "\nEnter commands:\n\n> ";
             std::getline(std::cin, input);
+
+            if (input[0] == ':')
+            {
+                std::string command = input;
+                std::string arg = "";
+                command.erase(0, 1);
+
+                int index = 0;
+                char c = command[index];
+
+                while (c != ' ' && index < command.length())
+                {
+                    index++;
+                    c = command[index];
+                }
+
+                int tmp_index = index;
+
+                if (command[index] == ' ')
+                    command.erase(index, 1);
+
+                while (index < command.length())
+                {
+                    arg += command[index];
+                    index++;
+                }
+
+                command.erase(tmp_index, command.length());
+
+                if (!command.empty())
+                {
+                    if (std::all_of(command.begin(), command.end(), ::isdigit))
+                    {
+                        int line = std::stoi(command);
+                        
+                        if (code.find(line) != code.end())
+                            code.at(line) = arg;
+                        else
+                            code.insert(std::pair<int, std::string>(line, arg));
+
+                        continue;
+                    } else if (command == "list" || command == "l")
+                    {
+                        for (std::pair<int, std::string> l : code)
+                        {
+                            std::cout << l.first << " " << l.second << std::endl;
+                        }
+                        continue;
+                    } else if (command == "run" || command == "r")
+                    {
+                        input = "";
+
+                        for (std::pair<int, std::string> l : code)
+                        {
+                            input += l.second + "\n";
+                        }
+                    }
+                }
+            }
 
             Node* program = tokenize(executable_path, program_path, input, program_path);
 
@@ -47,7 +110,7 @@ int main(int argc, char** argv)
 
             delete_nodes(program);
         }
-        while (input != "exit");
+        while (input != ":exit");
     } else
         std::cout << "Invalid parameters." << std::endl;
 
